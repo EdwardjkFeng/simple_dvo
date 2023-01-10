66ed2357082d1fd7b7675b5694e2e7c89ca6760b
Jingkun Feng <jingkun_feng@outlook.com>
2023-01-10 15:20:56 +0100

    update visualization viewpoint

--- scripts/pcd_utils.py
+++ scripts/pcd_utils.py
@@ -7,11 +7,7 @@ import copy
 def draw_registration_result_original_color(source, target, transformation):
     source_temp = copy.deepcopy(source)
     source_temp.transform(transformation)
-    o3d.visualization.draw_geometries([source_temp, target],
-                                      zoom=0.5,
-                                      front=[-0.2458, -0.8088, 0.5342],
-                                      lookat=[1.7745, 2.2305, 0.9787],
-                                      up=[0.3109, -0.5878, -0.7468])
+    o3d.visualization.draw_geometries([source_temp, target])
 
 
 def generate_o3d_pcd(size, rgb_path, depth_path, K, visualize=False):
